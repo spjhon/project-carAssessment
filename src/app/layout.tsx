201a7f76e89822f5ab708c4cc66305ce1d018f7b
Juan Aristizabal <spjhon@gmail.com>
2025-06-30 22:55:04 -0500

    se comienza a trabajar sobre el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en"  className="bg-amber-600">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
