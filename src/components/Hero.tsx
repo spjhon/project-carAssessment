d57fc4c1f9dc05e7df187db461424a9c614fc6d1
Juan Aristizabal <spjhon@gmail.com>
2025-07-08 22:29:42 -0500

    se le esta trabaando al layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,37 +7,55 @@ export const Hero = () => {
   return (
     <section className="
       container 
-      grid lg:grid-cols-2 place-items-center 
-      py-20 md:py-32 gap-10 
-      m-auto 
+      flex items-center
+      py-20
+      m-auto
       border-2 border-amber-400"
       >
+
       <div className="
-        text-center lg:text-start space-y-6 
-        border-2 border-blue-300 h-full"
-        >
+        text-center md:text-start space-y-6 
+        border-2 border-blue-300 h-full">
+
         <main className="text-5xl md:text-6xl font-bold">
+
           <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
+            <span className="
+              inline 
+              bg-gradient-to-r from-[#F596D3]  to-[#D247BF] 
+              text-transparent 
+              bg-clip-text">
               Shadcn
             </span>{" "}
             landing page
-          </h1>{" "}
-          for{" "}
+          </h1>
+
+          {" "}for{" "}
+
           <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
+            <span className="
+              inline 
+              bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] 
+              text-transparent 
+              bg-clip-text">
               React
-            </span>{" "}
+            </span>
+            {" "}
             developers
           </h2>
+
         </main>
 
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+        <p className="
+          text-xl text-muted-foreground 
+          md:w-10/12 
+          mx-auto lg:mx-0">
           Build your React landing page effortlessly with the required sections
           to your project.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
+
           <Button className="w-full md:w-1/3">Get Started</Button>
 
           <a
@@ -46,18 +64,22 @@ export const Hero = () => {
             target="_blank"
             className={`w-full md:w-1/3 ${buttonVariants({
               variant: "outline",
-            })}`}
-          >
+            })}`}>
+
             Github Repository
             <GitHubLogoIcon className="ml-2 w-5 h-5" />
+
           </a>
+
         </div>
+
       </div>
 
       {/* Hero cards sections */}
       <div className="z-10 border-2 border-blue-300 ">
         <HeroCards />
       </div>
+      
     </section>
   );
 };
